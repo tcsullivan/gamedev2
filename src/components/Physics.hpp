1f36f13ceafab93f28d0b2629d0e9804435969a6
Andy Belle-Isle <drumsetmonkey@gmail.com>
2019-09-07 01:11:57 -0400

    Added new physics component that allows entities to fall

--- src/components/Physics.hpp
+++ src/components/Physics.hpp
@@ -1,8 +1,6 @@
 /**
- * @file Player.hpp
- * Component for designating player-controlled entities.
- *
- * Copyright (C) 2019 Clyne Sullivan
+ * @file Physics.hpp
+ * Copyright (C) 2019  Belle-Isle, Andrew <drumsetmonkey@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,17 +16,16 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef COMPONENT_PLAYER_HPP_
-#define COMPONENT_PLAYER_HPP_
+#ifndef COMPONENT_PHYSICS_HPP_
+#define COMPONENT_PHYSICS_HPP_
 
 #include "Component.hpp"
 
-struct Player : Component<Player>
+struct Physics : Component<Physics>
 {
 public:
-    char _unused;
-
-    Player FromLua([[maybe_unused]] sol::object ref)
+    bool standing = true;
+    Physics FromLua([[maybe_unused]] sol::object ref)
     {
         return *this;
     }
@@ -37,9 +34,8 @@ public:
     void serialize([[maybe_unused]] cereal::JSONInputArchive& ar) final {}
 
     std::string serializeName(void) const final {
-        return "Player";
+        return "Physics";
     }
 };
 
-#endif // COMPONENT_PLAYER_HPP_
-
+#endif//COMPONENT_PHYSICS_HPP_
